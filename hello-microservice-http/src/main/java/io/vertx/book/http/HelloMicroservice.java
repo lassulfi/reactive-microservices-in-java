0d4842369e60ae450e361eb3cec1d42103cfe640
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-02-25 20:53:05 -0300

    Produzindo JSON

--- hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroservice.java
+++ hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroservice.java
@@ -1,20 +1,31 @@
 package io.vertx.book.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
+import io.vertx.ext.web.RoutingContext;
 
 public class HelloMicroservice extends AbstractVerticle {
 
     @Override
     public void start() {
     	Router router = Router.router(vertx);
-    	router.get("/").handler(rc -> rc.response().end("hello"));
-    	router.get("/:name").handler(rc -> rc.response()
-    			.end("hello " + rc.pathParam("name")));
+    	router.get("/").handler(this::hello);
+    	router.get("/:name").handler(this::hello);
     	
     	vertx.createHttpServer()
     	  .requestHandler(router::accept)
     	  .listen(8080);
     }
 
+    private void hello(RoutingContext rc) {
+    	String message = "hello";
+    	if(rc.pathParam("name") != null) {
+    		message += " " + rc.pathParam("name");
+    	}
+    	JsonObject json = new JsonObject().put("message", message);
+    	rc.response().putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+    	.end(json.encode());
+    }
 }
