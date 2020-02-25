5766a71a5f20b3044cf27a04caea7ed724ab1be4
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-02-25 19:44:10 -0300

    Usando rotas e parametros

--- hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroservice.java
+++ hello-microservice-http/src/main/java/io/vertx/book/http/HelloMicroservice.java
@@ -1,14 +1,19 @@
 package io.vertx.book.http;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class HelloMicroservice extends AbstractVerticle {
 
     @Override
-    public void start() {    	
+    public void start() {
+    	Router router = Router.router(vertx);
+    	router.get("/").handler(rc -> rc.response().end("hello"));
+    	router.get("/:name").handler(rc -> rc.response()
+    			.end("hello " + rc.pathParam("name")));
+    	
     	vertx.createHttpServer()
-    	  .requestHandler(req -> req.response()
-    			  .end("hello"))
+    	  .requestHandler(router::accept)
     	  .listen(8080);
     }
 
